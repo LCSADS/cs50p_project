8dabc9514b76cbf4c4175534abfb5bfb2b920a43
LCSADS <lucas.a.desimone@gmail.com>
2025-10-09 16:33:03 -0300

    Final structure implementation with UX menu to comply with CS50P 4 root-level functions requirement

--- main.py
+++ main.py
@@ -4,40 +4,44 @@ from storage import save_user, find_user
 from policies import security_feedback 
 from user_input import get_valid_password, get_valid_username
 from hasher import verify_password
+import sys
+def create_account():
+    print(f"New account :")
+    username = get_valid_username()
+    password = get_valid_password()
+    new_user = create_user(username, password)
+    if new_user:
+        save_user(new_user)
+        print(f"\nAccount '{username}' created.")
+def authentication():
+    username = input("Username: ").strip()
+    password = input("Password: ").strip()
+    login_attempt = login(username,password)
+    if login_attempt:
+        print(f"Welcome {username}!")
+        return True
+    return False
 
-print("\nconceito")
-print("\nCriação de usuário")
-username = get_valid_username()
-password = get_valid_password()
-
-new_user = create_user(username, password)
-
-if new_user:
-    save_user(new_user)
-    print("\nsucesso")
-else:
-    print("\nerro")
-
-login_success = login(username, password)
-
-if login_success:
-    print(f"\nlogin bem sucedido {login_success.username} recuperado do json")
-else:
-    print("\ndeu erro no json")
-print("\ntesting failure to login with wrong password")
-## failure test
-wrong_password = "whatever"
-invalid_password_login = login(username,wrong_password)
-if invalid_password_login is None:
-    print(f"\nSucessfully blocked login with wrong password")
-else:
-    print(f"\nfailure, login was successfull even with the wrong password")
+def menu():
+    while True:
+        print("\nMenu\n")
+        print("1 - Create new account")
+        print("2 - Login")
+        print("3 - Exit")
+        choice = input("Type the option number to select: ")
+        if choice == '1':
+            create_account()
+        elif choice == '2':
+            authentication()
+        elif choice == '3':
+            sys.exit("Bye!")
+        else:
+            print(f"Please select a valid option.")
 
 
-print("\ntesting failure to login with invalid username")
+def main():
+    menu()
 
-invalid_user_login = login("whatever",password)
-if invalid_user_login is None:
-    print(f"\nSucessfully blocked login with wrong username")
-else:
-    print(f"failure. login was successful with a invalid username")
+
+if __name__ == '__main__':
+    main()
